2809fc7076f9253bb8e30f1e0312c45f1c488edc
rmawogns <evolui@naver.com>
2023-10-05 20:57:45 +0900

    test9

--- app/index.ts
+++ app/index.ts
@@ -1,40 +1,46 @@
 import express from "express";
-import mongoose from "mongoose";
-import path from "path";
-import User from "./server/router/User";
-import Reple from "./server/router/Reple";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
+// import mongoose from "mongoose";
+// import path from "path";
+// import User from "./server/router/User";
+// import Reple from "./server/router/Reple";
+// import cookieParser from "cookie-parser";
+// import dotenv from "dotenv";
 
 const server = express();
-const port = 4001;
-dotenv.config({ path: ".env" });
+// const port = 4001;
+// dotenv.config({ path: ".env" });
 
-const MONGOURI = process.env.MONGOURI as string;
+// const MONGOURI = process.env.MONGOURI as string;
 
-server.use(express.static(path.join(__dirname, "./client/dist")));
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-server.use(cookieParser());
+// server.use(express.static(path.join(__dirname, "./client/dist")));
+// server.use(express.json());
+// server.use(express.urlencoded({ extended: true }));
+// server.use(cookieParser());
 
-server.use("/api/user", User);
-server.use("/api/reple", Reple);
+// server.use("/api/user", User);
+// server.use("/api/reple", Reple);
 
-server.listen(port, () => {
-  mongoose
-    .connect(MONGOURI)
-    .then(() => console.log("MongoDB 연결 성공"))
-    .catch((err) => console.log("Err", err));
-});
-
-server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
-});
-
-server.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/dist/index.html"));
-});
-
-// server.listen(4001, () => {
-//   console.log("connected");
+// server.listen(port, () => {
+//   mongoose
+//     .connect(MONGOURI)
+//     .then(() => console.log("MongoDB 연결 성공"))
+//     .catch((err) => console.log("Err", err));
 // });
+
+// server.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+// });
+
+// server.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/dist/index.html"));
+// });
+
+server.listen(4001, () => {
+  console.log("connected");
+});
+
+server.get("/hello", (req, res) => {
+  res.status(200).json({
+    message: "hello",
+  });
+});
